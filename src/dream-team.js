fb558055dae93b747b80f525ed62a2cc2a15772a
NnnastyaA <nastyafedorets@mail.ru>
2022-10-17 23:24:39 +0300

    add code

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let summaryLine = '';
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === "string" && members[i].replace(/ /g, '').length > 0) {
+      members[i] = members[i].replace(/ /g, '');
+      let firstLetter = members[i][0];
+      firstLetter = firstLetter.toUpperCase();
+      summaryLine = summaryLine + firstLetter;
+    }
+  }
+  return summaryLine.split('').sort().join('');
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,10 +12,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
-class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class DepthCalculator {
+  calculateDepth(a) {
+    var depth = 0; 
+    if (Array.isArray(a)) { 
+        for (var i in a) { 
+            depth = Math.max(depth, this.calculateDepth(a[i])); 
+        } 
+        depth++; 
+    } 
+    return depth; 
   }
 }
 
