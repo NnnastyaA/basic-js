26f1f92985c644bddf93b20e06644850d859202c
NnnastyaA <nastyafedorets@mail.ru>
2023-12-25 17:44:28 +0300

    feat: solve task expended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  // Set default values for options if not provided
+  const {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = options;
+
+  // Convert null to an empty string
+  const mainString = str === null ? 'null' : String(str);
+  const additionString = addition === null ? 'null' : String(addition);
+
+  // Repeat the addition string with the specified options
+  const repeatedAddition = new Array(additionRepeatTimes).fill(additionString).join(additionSeparator);
+
+  // Repeat the main string with the specified options, including the repeated addition
+  const repeatedString = new Array(repeatTimes).fill(mainString + repeatedAddition).join(separator);
+
+  return repeatedString;
 }
 
 module.exports = {
